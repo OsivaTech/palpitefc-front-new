9d7749bf471ce8c9702eed71c2a0626f265fe984
Adnilton <adniltonweb@gmail.com>
2024-07-26 01:19:17 -0300

    ajuste na typagem

--- src/http/prizes.ts
+++ src/http/prizes.ts
@@ -1,15 +1,15 @@
 'use server'
 import { get } from '@/lib/api'
 import { PrizesEndPoint } from '@/lib/endpoints'
-import { Prizes } from '@/types/Prizes'
+import { PrizesResponse } from '@/types/api/responses/PrizesResponse'
 
 export async function getPrizes() {
   try {
     const response = await get(PrizesEndPoint, { cache: 'no-cache' }, false)
-    const prizes: Prizes[] = await response.json()
+    const prizes: PrizesResponse[] = await response.json()
 
     return prizes || []
   } catch {
-    return [] as Prizes[]
+    return [] as PrizesResponse[]
   }
 }
--- src/http/rules.ts
+++ src/http/rules.ts
@@ -1,15 +1,15 @@
 'use server'
 import { get } from '@/lib/api'
 import { RulesEndPoint } from '@/lib/endpoints'
-import { Rules } from '@/types/Rules'
+import { RulesResponse } from '@/types/api/responses/RulesResponse'
 
 export async function getRules() {
   try {
     const response = await get(RulesEndPoint, { cache: 'no-cache' }, false)
-    const rules: Rules[] = await response.json()
+    const rules: RulesResponse[] = await response.json()
 
     return rules || []
   } catch {
-    return [] as Rules[]
+    return [] as RulesResponse[]
   }
 }
