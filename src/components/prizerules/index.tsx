c49a886f81c25c20d19aa08219a2b4f321709306
Francisco Augusto dos Santos <santos.frau@gmail.com>
2024-09-29 11:18:23 -0300

    feat(rules): adiciona regras de "Como funciona"

--- src/components/prizerules/index.tsx
+++ src/components/prizerules/index.tsx
@@ -17,15 +17,29 @@ const typePremium = (type: string, prize: string) => {
   }
   return prize
 }
+const getMedal = (place: number) => {
+  switch (place) {
+    case 1:
+      return '🥇'
+    case 2:
+      return '🥈'
+    case 3:
+      return '🥉'
+    default:
+      return '🎖️'
+  }
+}
+
 const renderPrizes = (prize: Prizes) => {
   return (
     <>
-      <h1 className="mb-3 text-center text-lg font-medium">{prize.title}</h1>
+      <h1 className="mb-3 text-lg font-medium">{prize.title}</h1>
       <div className="mb-7">
         {prize.prizes.map((prize, index) => (
-          <div key={index} className="flex justify-center mb-1">
+          <div key={index} className="flex mb-1">
             <h2 className="text-sm font-normal">
-              {prize.placing}º colocado: {typePremium(prize.type, prize.prize)}
+              {getMedal(prize.placing)} {typePremium(prize.type, prize.prize)}{' '}
+              para o {prize.placing}º lugar
             </h2>
           </div>
         ))}
@@ -39,19 +53,41 @@ const renderRules = (rules: Rules) => {
       <h1 className="mb-4 text-center text-xl font-medium">{rules.title}</h1>
       <div className="mb-6">
         {rules.rules.map((rule, index) => (
-          <div
-            key={index}
-            className="flex flex-col justify-center items-center mb-4"
-          >
-            <h2 className="text-lg font-bold text-center">{rule.name}</h2>
+          <div key={index} className="flex flex-col mb-4 gap-1">
+            <h2 className="text-lg font-medium">{rule.name}</h2>
+            <h2 className="text-sm font-normal">{rule.description}</h2>
+            <h2 className="text-base font-medium">{rule.complement}</h2>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
 
-            <h2 className="text-base font-normal text-center">
-              {rule.complement}
-            </h2>
+const renderHowItWorks = (howItWorks: Rules) => {
+  const groupedRules = howItWorks.rules.reduce(
+    (acc, rule) => {
+      acc[rule.name] = acc[rule.name] || []
+      acc[rule.name].push(rule.description)
+      return acc
+    },
+    {} as Record<string, string[]>,
+  )
 
-            <h2 className="text-sm font-normal text-center">
-              {rule.description}
-            </h2>
+  return (
+    <>
+      <h1 className="mb-4 text-center text-xl font-medium">
+        {howItWorks.title}
+      </h1>
+      <div className="mb-7">
+        {Object.entries(groupedRules).map(([name, descriptions], index) => (
+          <div key={index} className="flex flex-col mb-4 gap-1">
+            <h2 className="text-lg font-medium">{name}</h2>
+            {descriptions.map((description, i) => (
+              <h2 key={i} className="text-sm font-normal mb-1">
+                {description}
+              </h2>
+            ))}
           </div>
         ))}
       </div>
@@ -69,7 +105,13 @@ const PrizeRulesPage = ({ prize, rules }: PrizeRulesProps) => {
       <Separator className="mb-6 border border-white/50" />
       {prize.map((prize) => renderPrizes(prize))}
       <Separator className="my-6 border border-white/50 " />
-      {rules.map((rule) => renderRules(rule))}
+      {rules
+        .filter((r) => r.type === 'howItWorks')
+        .map((rule) => renderHowItWorks(rule))}
+      <Separator className="my-6 border border-white/50 " />
+      {rules
+        .filter((r) => r.type !== 'howItWorks')
+        .map((rule) => renderRules(rule))}
       <Separator className="my-6 border border-white/50 " />
     </div>
   )
