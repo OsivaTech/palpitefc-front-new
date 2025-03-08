40bb9a06237aae63abc8a439a849160a9515054e
Francisco Augusto dos Santos <santos.frau@gmail.com>
2025-03-08 09:33:55 -0300

    feat(layout): inclui scripts do GTM e remove facebook pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,27 +45,19 @@ export default function RootLayout({
           <AuthProvider token={token?.value}>
             <PageModalProvider>
               <body className="flex flex-col h-dvh bg-app-background">
+                <noscript>
+                  <iframe
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-MPCWHXKW"
+                    height="0"
+                    width="0"
+                    style={{ display: 'none', visibility: 'hidden' }}
+                  ></iframe>
+                </noscript>
+                <Script id="gtm" strategy="afterInteractive"></Script>
                 <Header />
                 {children}
                 <BottonMenu />
                 <Toaster />
-                <Script id="facebook-pixel" strategy="afterInteractive">
-                  {`
-                    !function (f, b, e, v, n, t, s) {
-                        if (f.fbq) return; n = f.fbq = function () {
-                            n.callMethod ?
-                                n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-                        };
-                        if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
-                        n.queue = []; t = b.createElement(e); t.async = !0;
-                        t.src = v; s = b.getElementsByTagName(e)[0];
-                        s.parentNode.insertBefore(t, s)
-                    }(window, document, 'script',
-                        'https://connect.facebook.net/en_US/fbevents.js');
-                    fbq('init', '919791843493604');
-                    fbq('track', 'PageView');
-                  `}
-                </Script>
                 <Script id="hotjar" strategy="afterInteractive">
                   {`
                     (function(h,o,t,j,a,r){
@@ -86,6 +78,15 @@ export default function RootLayout({
         </CookiesProvider>
       </NextIntlClientProvider>
       <GoogleAnalytics gaId="G-XH7QPE19PE" />
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MPCWHXKW');
+        `}
+      </Script>
     </html>
   )
 }
