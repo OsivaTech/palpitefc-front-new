ca04067758b788ac654faca1471e9ed56d9e6df0
Francisco Augusto dos Santos <santos.frau@gmail.com>
2025-03-28 21:28:02 -0300

    remove mascara no custom input para resolver erro

--- src/components/custom-input/index.tsx
+++ src/components/custom-input/index.tsx
@@ -1,5 +1,4 @@
 import React, { InputHTMLAttributes } from 'react'
-import ReactInputMask from 'react-input-mask'
 
 interface CustomInputProps extends InputHTMLAttributes<HTMLInputElement> {
   mask?: string
@@ -11,67 +10,35 @@ interface CustomInputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const CustomInput = React.forwardRef<HTMLInputElement, CustomInputProps>(
-  ({ className, type, mask, maskChar, icon, value2, label, ...props }, ref) => {
-    if (mask) {
-      return (
-        <ReactInputMask mask={mask} maskChar={maskChar} {...props}>
-          <div className="relative w-full">
-            {label && (
-              <label className="block text-white text-sm font-medium">
-                {label}
-              </label>
-            )}
-            <div className="relative">
-              {icon && (
-                <span className="absolute inset-y-0 left-0 bottom-2 flex items-center pl-3">
-                  {icon}
-                </span>
-              )}
-              <input
-                {...props}
-                ref={ref}
-                type={type}
-                className={`${icon ? 'pl-10' : ''} w-full bg-transparent border-b border-white h-[32px] placeholder-white/70 font-medium text-xs ${className}`}
-              />
-              {value2 && (
-                <span className="absolute inset-y-0 right-0 flex items-center pl-3 text-white px-[20px] font-medium text-xs">
-                  {value2}
-                </span>
-              )}
-            </div>
-          </div>
-        </ReactInputMask>
-      )
-    } else {
-      // Fallback for when no mask is provided
-      return (
-        <div className="relative w-full">
-          {label && (
-            <label className="block text-white text-sm font-medium">
-              {label}
-            </label>
+  ({ className, type, icon, value2, label, ...props }, ref) => {
+    const renderInput = (inputProps: InputHTMLAttributes<HTMLInputElement>) => (
+      <div className="relative w-full">
+        {label && (
+          <label className="block text-white text-sm font-medium">
+            {label}
+          </label>
+        )}
+        <div className="relative">
+          {icon && (
+            <span className="absolute inset-y-0 left-0 bottom-2 flex items-center pl-3">
+              {icon}
+            </span>
           )}
-          <div className="relative">
-            {icon && (
-              <span className="absolute inset-y-0 left-0 bottom-2 flex items-center pl-3">
-                {icon}
-              </span>
-            )}
-            <input
-              {...props}
-              ref={ref}
-              type={type}
-              className={`${icon ? 'pl-10' : ''} w-full bg-transparent border-b border-white h-[32px] placeholder-white/70 font-medium text-xs ${className}`}
-            />
-            {value2 && (
-              <span className="absolute inset-y-0 right-0 flex items-center pl-3 text-white px-[20px] font-medium text-xs">
-                {value2}
-              </span>
-            )}
-          </div>
+          <input
+            {...inputProps}
+            ref={ref}
+            type={type}
+            className={`${icon ? 'pl-10' : ''} w-full bg-transparent border-b border-white h-[32px] placeholder-white/70 font-medium text-xs ${className}`}
+          />
+          {value2 && (
+            <span className="absolute inset-y-0 right-0 flex items-center pl-3 text-white px-[20px] font-medium text-xs">
+              {value2}
+            </span>
+          )}
         </div>
-      )
-    }
+      </div>
+    )
+    return renderInput(props)
   },
 )
 
